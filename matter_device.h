8ca405fd7cadf112aea8ffa21b6259646b87016c
hencou <32905287+hencou@users.noreply.github.com>
2026-06-17 08:37:18 +0200

    Delete matter_device.h

--- matter_device.h
+++
@@ -1,7 +0,0 @@
-## esp-matter wordt geleverd als git-submodule clone, niet via component-manager.
-## Zie INSTALL.md.
-dependencies:
-  idf:
-    version: ">=5.2"
-  espressif/mdns:
-    version: "*"
